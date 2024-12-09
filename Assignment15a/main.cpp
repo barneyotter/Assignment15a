7e2e84563ccfac3435db43511f55fba36b5aae2f
barneyotter <128318050+barneyotter@users.noreply.github.com>
2024-12-08 21:08:13 -0500

    main.cpp

--- Assignment15a/main.cpp
+++ Assignment15a/main.cpp
@@ -1,2 +1,44 @@
 // Maia Barnhart
 // CIS 1202 501
+
+#include <iostream>
+#include "exceptions.h"
+
+using namespace std;
+
+char character(char start, int offset);
+
+int main() {
+    
+    try {
+        cout << "character('a', 1) = " << character('a', 1) << endl;
+    } catch (const exception& e) {
+        cout << e.what() << endl;
+    }
+    
+    try {
+        cout << "character('a', -1) = " << character('a', -1) << endl;
+    } catch (const exception& e) {
+        cout << e.what() << endl;
+    }
+    
+    try {
+        cout << "character('Z', -1) = " << character('Z', -1) << endl;
+    } catch (const exception& e) {
+        cout << e.what() << endl;
+    }
+    
+    try {
+        cout << "character('?', 5) = " << character('?', 5) << endl;
+    } catch (const exception& e) {
+        cout << e.what() << endl;
+    }
+    
+    try {
+        cout << "character('A', 32) = " << character('A', 32) << endl;
+    } catch (const exception& e) {
+        cout << e.what() << endl;
+    }
+    
+    return 0;
+}
